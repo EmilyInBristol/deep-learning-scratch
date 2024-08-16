515667a6420d7e267f808b9dfc16fc201c9533a7
EmilyInBristol <echocx1234@gmail.com>
2024-08-16 22:25:19 +0100

    update cnn.py

--- cnn.py
+++ cnn.py
@@ -28,37 +28,55 @@ class conv2D(nn.Module):
     def forward(self, x):
         return corr2d(x, self.weight) + self.bias
     
-X = torch.ones((6, 8))
-X[:,2:6] = 0
-kernal = torch.tensor([[1.0, -1.0]])
+def demo_conv():
+    X = torch.ones((6, 8))
+    X[:,2:6] = 0
+    kernal = torch.tensor([[1.0, -1.0]])
 
-Y = corr2d(X, kernal)
+    Y = corr2d(X, kernal)
 
-epoch = 10
-model = nn.LazyConv2d(1, kernel_size=(1,2), bias=False)
+    epoch = 10
+    model = nn.LazyConv2d(1, kernel_size=(1,2), bias=False)
 
-X = X.reshape((1, 1, 6, 8))
-Y = Y.reshape((1, 1, 6, 7))
-for i in range(epoch):
-    #model = conv2D(kernal.shape)
+    X = X.reshape((1, 1, 6, 8))
+    Y = Y.reshape((1, 1, 6, 7))
+    for i in range(epoch):
+        #model = conv2D(kernal.shape)
 
-    y_hat = model(X)
+        y_hat = model(X)
 
-    loss = (y_hat - Y) ** 2
-    loss = loss.sum()
-    model.zero_grad()
+        loss = (y_hat - Y) ** 2
+        loss = loss.sum()
+        model.zero_grad()
 
-    loss.backward()
-    model.weight.data[:] -= model.weight.grad * 0.03
+        loss.backward()
+        model.weight.data[:] -= model.weight.grad * 0.03
 
-    if i % 2 == 0:
-        print(f'i:{i} loss:{loss}')
+        if i % 2 == 0:
+            print(f'i:{i} loss:{loss}')
 
-print(model.weight.data.reshape(1,2))
+    print(model.weight.data.reshape(1,2))
 
 
+def comp_conv2d(conv2d, X):
+    X = X.reshape((1, 1) + X.shape) # add exmpale and channel dimention
+    Y = conv2d(X)
+    Y = Y.reshape(Y.shape[2:])
 
-    
+"""
+conv2d = nn.LazyConv2d(1, padding=1, kernel_size=3)
+X = torch.rand(size = (8,8))
+ret = comp_conv2d(conv2d, X)
+#print(ret.shape)
+"""
+
+X = torch.tensor([[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
+               [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]])
+K = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]]])
+
+Y = sum(corr2d(x, k) for x, k in zip(X, K))
+print(Y)
+
 
     
     
