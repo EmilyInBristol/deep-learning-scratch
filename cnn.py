7c8d17e045bbc9f03431b089dcf2c39647d721c5
EmilyInBristol <echocx1234@gmail.com>
2024-08-24 22:30:21 +0100

    add resnet block

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ from torch import nn # type: ignore
 from model import FashionMNIST, train_loop
 import torch.optim as optim # type: ignore
 import matplotlib.pyplot as plt # type: ignore
+from torch.nn import functional as F # type: ignore
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -164,6 +165,30 @@ class AlexNet(nn.Module):
 
     def forward(self, X):
         return self.net(X)
+    
+class Residual(nn.Module):
+    def __init__(self, num_channels, use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.LazyConv2d(num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.LazyConv2d(num_channels, kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.LazyConv2d(num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+
+        self.bn1 = nn.LazyBatchNorm2d()
+        self.bn2 = nn.LazyBatchNorm2d()
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+
+        Y += X
+        return F.relu(Y)
+
+        
 
 def layer_summary(X_shape, model):
     X = torch.randn(X_shape)
