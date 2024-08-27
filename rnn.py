8cc57eeed95921bd9aa83696e4ef307c8363f5bd
EmilyInBristol <echocx1234@gmail.com>
2024-08-27 12:10:25 +0100

    synthesize data for rnn model

--- rnn.py
+++ rnn.py
@@ -1,9 +1,17 @@
 import torch # type: ignore
 from torch import nn # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 
 class Data(nn.Module):
     def __init__(self, T=10, num_train=60):
-        pass
+        self.time = torch.arange(1, T+1, dtype=torch.float32)
+        self.x = torch.sin(0.01 * self.time) + torch.randn(T) * 0.2
 
-time = torch.arange(1, 11, dtype=torch.float32)
-print(time)
+
+data = Data(T=1000)
+
+plt.plot(data.time, data.x)
+plt.xlabel('Time')
+plt.ylabel('X')
+plt.legend()
+plt.show()
