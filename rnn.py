7d60edf94f567191c00225434aa9d41f5721cfa4
EmilyInBristol <echocx1234@gmail.com>
2024-08-28 09:13:16 +0100

    update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,17 +1,38 @@
 import torch # type: ignore
 from torch import nn # type: ignore
 import matplotlib.pyplot as plt # type: ignore
+from model import train_model, LinearRegression
 
 class Data(nn.Module):
-    def __init__(self, T=10, num_train=60):
+    def __init__(self, T=1000, num_train=600, tau=4):
         self.time = torch.arange(1, T+1, dtype=torch.float32)
         self.x = torch.sin(0.01 * self.time) + torch.randn(T) * 0.2
+        self.tau = tau
+        self.T = T
+        self.num_train = num_train
+
+
+    def get_dataloader(self, train=True):
+        features = [self.x[i: self.T-self.tau+i] for i in range(self.tau)]
+        self.features = torch.stack(features, 1)
+        self.labels = self.x[self.tau:].reshape(-1, 1)
+        i = slice(0, self.num_train) if train else slice(self.num_train, None)
+        return self.features[i], self.labels[i]
 
 
 data = Data(T=1000)
+train_x, train_y = data.get_dataloader()
+model = LinearRegression(0.01)
+train_model(model, train_x, train_y)
 
+
+
+
+
+"""
 plt.plot(data.time, data.x)
 plt.xlabel('Time')
 plt.ylabel('X')
 plt.legend()
 plt.show()
+"""
